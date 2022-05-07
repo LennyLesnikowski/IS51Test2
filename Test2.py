bf1a64bf0765320814b4fb0ee8a58cd98521949a
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-05-06 23:41:06 -0700

    Rushing my psudocode because I took to long, but it's done

--- Test2.py
+++ Test2.py
@@ -1,3 +1,27 @@
+"""
+start
+First open the text file in read mode.
+The for every line in a new list remove all the characters from the right in the list and then.
+create a new variable for this list of integesters by mapping each one as an integer to the list.
+The number of grades is simply the length of this new integer based list.
+the average of these grades is simply the sum of the list divided by the length of the list.
+print the number of grades & the average grade to the consol.
+
+Create a new definition
+    Create a blank list
+    Assign a value to the score to be calculated 
+    For every variable in the list of grades, if the variable is greater than average.
+    Add that variable to a new list. of above avg grades
+    Calculate the percentage of grades that are above average by dividing the length of the above average scores list by the length of the regular scores.
+    Round the final percent to two decimal places, as is standard.
+    Print that final percent to the consol
+Call that definition
+end
+
+
+
+"""
+
 
 
 scoreList = open("Final.txt", "r")
